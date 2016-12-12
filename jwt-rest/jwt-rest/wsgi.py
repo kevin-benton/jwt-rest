d1bc331ac20d90fb39bebea31ab1c59519ae183d
Kevin Benton <krbenton@windstream.net>
2016-12-11 21:42:01 -0600

    Update comments from previous project name.

--- jwt-rest/jwt-rest/wsgi.py
+++ jwt-rest/jwt-rest/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for lambdatext project.
+WSGI config for jwt-rest project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lambdatext.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "jwt-rest.settings")
 
 application = get_wsgi_application()
